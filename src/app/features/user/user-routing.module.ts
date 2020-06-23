56b30ba65ddc6d8c99dc84ee98ba360e4e1ce9b4
Jovan Kron <jovankron97@hotmail.com>
2020-06-23 18:01:03 +0200

    no-guard za login i registraciju

--- src/app/features/user/user-routing.module.ts
+++ src/app/features/user/user-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { rootPaths } from '../../core/constants/root-paths';
 import { RegisterComponent } from './pages/register/register.component';
+import { NoGuard } from '../../core/guards/no.guard';
 
 
 const routes: Routes = [
-  {path: rootPaths.login, component: LoginComponent},
-  {path: rootPaths.register, component: RegisterComponent}
+  {path: rootPaths.login, component: LoginComponent, canActivate:[NoGuard]},
+  {path: rootPaths.register, component: RegisterComponent, canActivate:[NoGuard]}
 
 ];
 
