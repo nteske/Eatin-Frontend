a34a58525910f3a9ad0b8ea166e233881ade0f96
nteske <nemanjatesic96@gmail.com>
2020-06-28 03:58:42 +0200

    heroku 5

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
+app.set('views', __dirname + '/dist/frontend');
+app.set('view engine', 'html');
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/frontend'));
 
